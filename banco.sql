7f9714b57aec67faee426b77ea32c6928c694f88
Alexandre Junior <gamerapsj@gmail.com>
2023-09-16 13:43:37 -0300

    Fazendo alterações finais

--- banco.sql
+++ banco.sql
@@ -33,8 +33,7 @@ CREATE TABLE filme (
 INSERT INTO filme (nome, valor, quantidadeEstoque, imagem, descricao)
 VALUES
   ('Planeta dos Macacos', 12.99, 2, 'https://i.pinimg.com/originals/c3/75/04/c37504d59ef60f537ae76033a9af0f76.jpg', "ola"),
-  ('O Senhor dos Anéis', 14.99, 3, 'https://img.elo7.com.br/product/original/269274A/poster-o-senhor-dos-aneis-a-sociedade-do-anel-lo04-90x60-cm-presente-geek.jpg', "ola"),
-  ('Harry Potter', 16.99, 4, 'https://http2.mlstatic.com/D_NQ_NP_672586-MLB27566127405_062018-O.webp', "ola");
+  ('O Senhor dos Anéis', 14.99, 3, 'https://img.elo7.com.br/product/original/269274A/poster-o-senhor-dos-aneis-a-sociedade-do-anel-lo04-90x60-cm-presente-geek.jpg', "ola");
   INSERT INTO filme (nome, valor, quantidadeEstoque, imagem, descricao)
 VALUES
   ('Star Wars', 15.99, 5, 'https://img.elo7.com.br/product/original/2C25D05/big-poster-filme-star-wars-o-despertar-da-forca-tam-90x60-cm-poster-star-wars.jpg', "ola"),
--- conn.php
+++ conn.php
@@ -1,7 +1,9 @@
 <?php
 $air_servername = "localhost";
 $air_username = "root";
-$air_password = "root";
+$air_password = "";
+// #Se estiver usando my sql com a senha root
+// $air_password = "root";
 $air_dbname = "locadora";
 // Create connection
 $air_conn = mysqli_connect($air_servername, $air_username, $air_password, $air_dbname);
